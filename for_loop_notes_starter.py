26c25e2df01e82136f61acc7c527cab93ee020bf
Matt Callaghan <mcallaghan@naperville203.org>
2020-12-07 20:38:14 -0600

    Update for_loop_notes_starter.py

--- for_loop_notes_starter.py
+++ for_loop_notes_starter.py
@@ -1,39 +1,35 @@
-# Intro to for loops notes
-#   counting with the range() function 
+# Intro to for loops notes - counting with the range() function 
 
-# counting with while loops:
+# Counting with while loops:
 # CHALLENGE: Print 'Hello, world.' 10 times using a while loop
 
 # FOR LOOP - 
 
-# pseudocode:
+# For Loop pseudocode:
  
 # Print "Hello, world." 10 times using a for loop
-input('\nPress enter to begin.')
+input('\nPrint "Hello, world." 10 times... Press enter to begin.')
 
- 
 # Display numbers 1-10 using a for loop
-input("\nPress enter to begin.")
+input("\nPrinting 1 - 10... Press enter to begin.")
 
-# range() function 
-
-# TUPLE: 
+# range() function:
 
 #   Display numbers 1-20
-input("\nPress enter to begin.")
+input("\nPrinting 1 - 20... Press enter to begin.")
  
 # Controlling range() in more detail
 #   Count 10-20, by 2's
-input("\nPress enter to begin.")
+input("\nPrinting 10 - 20, by 2's... Press enter to begin.")
  
 # Practice Challenges:
 #   Increment challenge:  Count by 3’s from 3-99, inclusive
-input("\nPress enter to begin.")
+input("\nCount by 3’s from 3-99, inclusive... Press enter to begin.")
 
 #   Counting Down Challenge: Count down by 2’s from 10 to -10, inclusive
-input("\nPress enter to begin.")
+input("\nCount down by 2’s from 10 to -10, inclusive... Press enter to begin.")
 
-# Application Challenges:
+# Application Challenges: Use the range function and for loops to solve each prompt.
 
 # Bug Collector Program -
 #   A bug collector collects bugs every day for 7 days. Write a program that
@@ -41,20 +37,21 @@ input("\nPress enter to begin.")
 #   The loop should ask for the number of bugs collected for each day,
 #   and when the loop is finished, the program should display the total
 #   number of bugs collected. Can you get the day number to display each day?
-
+input("\nBug Collector... Press enter to begin.")
  
-# Calories Burned Program-
+# Calories Burned Program -
 #   Running on a treadmill you burn 3.9 calories per minute.
 #   Write a program that uses a loop to display the number of
 #   calories burned after 10, 15, 20, 25, and 30 minutes.
 #   Display the number of minutes without hard-coding them.
-
+input("\nCalories Burned... Press enter to begin.")
  
-# Advanced Challenge
-# Pennies For Pay Program-
+
+# Pennies For Pay Program -
 #   Write a program that calculates the amount of money a person would
 #   earn over a period of time if their salary is one penny the first day,
 #   two pennies the second day, and continues to double each day.
 #   The program should ask the user for the number of days worked.
 #   Display a table showing what the salary was for each day,
 #   and then show the total pay at the end of the period.
+input("\nPennies For Pay... Press enter to begin.")
