1eaa1ef002e6ac78a42605470c4378a965f30902
Matt Callaghan <mcallaghan@naperville203.org>
2020-12-13 13:44:41 -0600

    Update turtle_notes_finished.py

--- turtle_notes_finished.py
+++ turtle_notes_finished.py
@@ -5,7 +5,7 @@
 import turtle
 
 # INDEX VALUE - selects an item from a sequence, such as a character from a string, or an element from a tuple.
-#   use square bracket [] to select an item by index value
+#   Use square brackets (e.g. []) to select an item by index value
 """
                0  1  2  3  4  5     ( positive indices start at 0, always the first element )
     string = " T  U  R  T  L  E "
@@ -19,7 +19,8 @@ print("R:", string[2], string[-4])
 print("T:", string[-3], string[3])
 print("L:", string[-2])
 print("E:", string[-1]) # -1 always the last element
-#print(string[len(string)])  # INDEX ERROR - index out of range; len returns 4
+# INDEX ERROR - index value accesed is out of range or does not exist
+#print(string[len(string)])  # len returns 6, but last index value is 5; 
 
 #            0           1            2              3
 NAMES = ("Leonardo", "Raphael", "Michelangelo", "Donatello") # all caps variables (CONSTANTS) never change
@@ -30,21 +31,19 @@ print(names[0] + " is the color " + colors[0])
 
 print()
 # accessing index values within an loop
-#   EXTREMELY common line of code to access index values of one or more sequences
+#   EXTREMELY common line of code to access index values of a sequence
+#   Useful for printing sequences of equal length in columns
 for index in range(len(names)):
     print(names[index] + " is the color " + colors[index])
 
 # Object Oriented Programming (OOP) refresher 
 #   OBJECT - a software model that bundles data and behavior together; useful for modeling real-world objects, like turtles!
-#   METHOD - behaviors, or actions, of a software object
-#       RECALL: Think of and write down below one example of an object in Alice. 
-#               Write down two examples of methods for this object.
-#       e.g. in Alice a <penguin> was an object and a <penguin's> methods included <"say" and "move">
-#       e.g. in Python a <Turtle> is an object and a turtle's methods include moving <forward() & right()>
+#   METHOD - behaviors, or actions, of a software object (a turtle can move forward, turn right, or change its color)
+#   ATTRIBUTES - charactaristics, or data, associated with an object (AKA instance variables) (a turtle has a color, a position, and shape)
 
 # create Leonardo
 leo = turtle.Turtle()   # create Turtle object from turtle module by calling the class Turtle
-# I didn't write the code for a Turtle. How do I know how to use it?
+# We didn't write the code for a Turtle. How do we know how to use it?
 #   Google "Python docs turtle" for Turtle documentation
 leo.shape("turtle")     # run method on object to change shape
 leo.color(colors[0])    # run method on object to change color
