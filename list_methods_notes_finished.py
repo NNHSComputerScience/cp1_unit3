49d2f63d28a54ff5122d528e9cd18cb3c2db8c68
Matt Callaghan <mcallaghan@naperville203.org>
2022-11-14 10:32:47 -0600

    Update list_methods_notes_finished.py

--- list_methods_notes_finished.py
+++ list_methods_notes_finished.py
@@ -1,75 +1,87 @@
-# List Methods Notes
-# METHOD - an ability or behavior that an object has; something it can do.
-#   Like strings and turtles, lists are objects that have their own behavior, 
-#     or list methods.
-import random
+"""
+Name: Mr. C
+Title: List Methods Notes
+Description: Overview of the list methods you need to know and how to use them.
 
+METHOD - an ability or behavior of an object.
+
+Like strings and turtles, lists are objects that have their own methods.
+"""
+import random
 row = ["Ford", "Audi", "BMW", "Lexus", "Mercedes", "Jeep"]
+print (row)
+
+# concatenation is one way to add cars to a list, but it does not mutate the list
+row += ["Ford", "Tesla"]
 print(row)
 
-row += ["Ford", "Tesla"] # concatenation is one way to add item(s) to a list, but it doesn't mutate the list
-
-# LIST METHODS mutate the list
+# LIST METHODS mutate lists:
 
 # 1: Append Method
 #   adds an element to the end of a list
-row.append("Ford")
+row.append("BMW")
 print(row)
 
-# A list can only append one item at a time.
-#row.append("Ford", "Buick") # error
-#print(row) 
-
-car = 1
-while car:
-    car = input("Enter a car to add (leave blank to exit): ").title()
-    if car:
-        row.append(car)
+# we can only append one item at a time
+#row.append("Toyota", "Ford") 
+# ERROR: TypeError: append() takes exactly one argument (2 given)
 
 # 2: Pop method
 #   removes an element by index value from a list and returns it
-row.pop(0)
+#   try to... remove the first car in the row
+car1 = row.pop(0)
+car2 = row.pop(0)
 print(row)
+print("Returned cars:", car1, car2)
+
 
 # 3: Remove method
-#   removes an element from a list (a value)
-#   del/pop removes an index value
+#   removes the first occurence of an element from a list
+#   try to... remove the BMW
 row.remove("BMW")
 print(row)
-
-# this method removes the first occurance of an element only
-row.remove("Ford")
+row.remove("BMW")
 print(row)
-
+ 
 # 4: Sort method
 #   sorts a list in ascending order
+#	strings sort alphabetically, numbers numerically
+#   try to... sort the list in ascending order
 row.sort()
 print(row)
 
 # 5: Reverse method
 #   reverses the order of a list
-row.reverse()
-print(row)
+#   try to... sort the list in descending order
 row.reverse()
 print(row)
 
 # 6: Count method
 #   counts the number of occurances of an element
-print(row.count("Ford"))
+#   try to... count the number of "Ford" cars in the row
+row.append("Ford")
+row.append("Ford")
+print(row)
+num_fords = row.count("Ford")
+print("Number of Fords:", num_fords)
+print(row)
 
 # 7: Index method
 #   displays the index value of an element
-print(row.index("Lexus"))
+#   try to... display the index value of the "Jeep"
+jeep_index = row.index("Jeep")
+print("Jeep index is:", jeep_index)
 
 # 8: Insert method
 #   inserts an element at a specific index value
-row.insert(2, "Kia")
+#   try to... insert a "Harley Davidson" in the second parking spot
+print(row)
+row.insert(1, "Harley Davidson")
 print(row)
 
 # random.choice(sequence) function
-#   Return a random element from the non-empty sequence seq. If seq is empty, raises IndexError.
-#       e.g.
-rand_car = random.choice(row)
-rand_vowel = random.choice(“AEIOU”)
+#	Return a random element from a sequence, as long as there are items left in the sequence.
 
-print("Random car", rand_car, "\nRandom vowel", rand_vowel)
+rand_car = random.choice(row)
+print("A random car:", rand_car)
+print("A random vowel:", random.choice("AEIOU"))
